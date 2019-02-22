0f4ff90d71d05e4d456e5247fb6a13dbe4f696ac
Nicolette Borrelli <nicole@hitori.org>
2019-02-21 22:08:06 -0800

    Add parameter to the script.
    At least now you can pass the event ID as a parameter, rather than
    hard coding it in the file.
    
    The ROM name is still hard coded, but ¯\_(ツ)_/¯

--- edump.py
+++ edump.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
+import sys
+
 # Global to hold the ROM data (for now)
 rom_data = bytes()
 
 def lookup_event(id):
-    print("ROM has len: " + str(len(rom_data)))
     if id >= 0x1388 and id <= 0x1F3F:
         lut_id_offset = 0x1388
         lut_base = 0x7788
@@ -17,12 +18,20 @@ def lookup_event(id):
     # first two bytes.
     return (rom_data[lut_addr + 1] << 8) + rom_data[lut_addr]
 
-def main():
+def main(argv):
+    if len(argv) != 1:
+        raise ValueError("Please pass event ID as a single parameter")
+
+    if (argv[0].startswith("0x")):
+        event_id = int(argv[0], 0)
+    else:
+        event_id = int(argv[0], 16)
+
     with open("ff-dos.gba", "rb") as binary_file:
         global rom_data
         # Read the whole file at once
         rom_data = binary_file.read()
-    addr = lookup_event(0x138C)
+    addr = lookup_event(event_id)
 
     # Events start with a command, followed by a lengthself.
     # The important one is '0', for now, which is "end".
@@ -35,4 +44,4 @@ def main():
         addr = addr + rom_data[addr + 1]
 
 if __name__== "__main__":
-  main()
+  main(sys.argv[1:])
