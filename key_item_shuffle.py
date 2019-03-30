b018b54c4cb43e08236e3a0a8cd95d08f6fd35b6
jomazeika <jmazeika@ucsc.edu>
2019-03-30 12:27:31 -0700

    Adding in the framework for KI shuffle; depends on Clingo 4.5 or bettter

---
+++ key_item_shuffle.py
@@ -0,0 +1,29 @@
+#  Copyright 2019 Stella Mazeika
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from subprocess import run, PIPE
+from math import pow
+import json
+
+"""Note: this requires an installation of Clingo 4.5 or better"""
+
+def shuffle_key_items(rng):
+    solving_seed = int(rng.random() * pow(2,32))
+    command = ["clingo", "KeyItemSolving.lp", "KeyItemData.lp"]
+    command.append("--sign-def=3")
+    command.append("--seed="+str(solving_seed))
+    command.append("--outf=2")
+    clingo_out = json.loads(run(command, stdout=PIPE).stdout)
+    pairings = clingo_out['Call'][0]['Witnesses'][0]['Value']
+    
+    #All pairings are of the form "pair(item,location)" - need to parse the info
